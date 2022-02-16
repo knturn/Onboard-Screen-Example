bacb9a71f12bf427c4c2e869190064f6cdd94be2
Kaan Turan <dyt.kaanturan@gmail.com>
2022-02-16 21:53:31 +0300

    splash page created

---
+++ splash_views/lib/splash_page_views/splash_page_views.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashPageView extends StatefulWidget {
+  const SplashPageView({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageViewState createState() => _SplashPageViewState();
+}
+
+class _SplashPageViewState extends State<SplashPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text("Looooo"),
+      ],
+    );
+  }
+}
